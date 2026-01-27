bd8abe6460c522a4a9c1954e08a23690a1232cad
Farheen <farheen.ahmed2004@gmail.com>
2026-01-27 21:27:50 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Database connected successfully to ${connectionInstance.connection.host}`);
+    } catch (err) {
+        console.error("Error connecting to the database", err);
+        process.exit(1);
+    }
+}
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello, World!');
+import dotenv from 'dotenv'
+
+import connectDB from './db/index.js';
+//import express from "express";
+
+dotenv.config({
+    path: './.env'
+})
+connectDB();
+
+//const app=express();
+// ( async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error",(err)=>{
+//             console.log("Error in app", err);
+//             throw err;
+//         });
+
+//         app.listen(process.env.PORT || 3000,()=>{
+//             console.log(`Server is running on port ${process.env.PORT || 3000}`);
+//         });
+//     }
+//     catch(err){
+//         console.log("Error in DB connection", err);
+//     }
+// })()
